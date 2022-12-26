bea28167f61dc32e3a883225aca5314c12d98502
Azizul Hakim <aziz.swe71@gmail.com>
2022-12-27 01:06:13 +0600

    working with toggle buttn and TabBar and switch widget

--- flutter_bitm/lib/screens/expanded_widget.dart
+++ flutter_bitm/lib/screens/expanded_widget.dart
@@ -26,220 +26,241 @@ class _ExpandedWidgetState extends State<ExpandedWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        title: Text('Expanded Widget'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(12),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.topCenter,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(15)),
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.yellow,
-                              borderRadius: BorderRadius.circular(15)),
-                          height: 80,
-                          width: double.infinity,
-                          child: Center(
-                            child: ToggleButtons(
-                              fillColor: Colors.white,
-                              borderWidth: 3,
-                              borderColor: Colors.white,
-                              selectedColor: Colors.green,
-                              color: Colors.blue,
-                              selectedBorderColor: Colors.red,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8)),
-                              isSelected: _selectedFruits,
-                              children: fruits,
-                              onPressed: (index) {
-                                setState(() {
-                                  _selectedFruits[index] =
-                                      !_selectedFruits[index];
-                                });
-                              },
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          title: Text('Expanded Widget'),
+          bottom: TabBar(tabs: [
+            Tab(    
+              icon: Icon(Icons.home),
+              text: 'Home',
+            ),
+            Tab(
+              icon: Icon(Icons.person),
+              text: 'People',
+            ),
+            Tab(
+              icon: Icon(Icons.share),
+              text: 'Share',
+            ),
+            Tab(
+              icon: Icon(Icons.settings),
+              text: 'Setting',
+            ),
+          ]),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(15)),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.circular(15)),
+                            height: 80,
+                            width: double.infinity,
+                            child: Center(
+                              child: ToggleButtons(
+                                fillColor: Colors.white,
+                                borderWidth: 3,
+                                borderColor: Colors.white,
+                                selectedColor: Colors.green,
+                                color: Colors.blue,
+                                selectedBorderColor: Colors.red,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)),
+                                isSelected: _selectedFruits,
+                                children: fruits,
+                                onPressed: (index) {
+                                  setState(() {
+                                    _selectedFruits[index] =
+                                        !_selectedFruits[index];
+                                  });
+                                },
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(15)),
-                              height: 80,
-                              child: Center(child: Text('2')),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(15)),
+                                height: 80,
+                                child: Center(child: Text('2')),
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.yellow,
-                                  borderRadius: BorderRadius.circular(15)),
-                              height: 80,
-                              child: Center(child: Text('3')),
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.yellow,
+                                    borderRadius: BorderRadius.circular(15)),
+                                height: 80,
+                                child: Center(child: Text('3')),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(15)),
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(15)),
-                          height: 200,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TextWidget(
-                                name: "Salman",
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              TextWidget(
-                                name: "Salman",
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              TextWidget(
-                                name: "Salman",
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.yellow,
-                              borderRadius: BorderRadius.circular(15)),
-                          height: 200,
-                          child: Center(
-                              child: TextWidget(
-                            name: "kamal",
-                          )),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.topCenter,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(15)),
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.yellow,
-                              borderRadius: BorderRadius.circular(15)),
-                          // height: 80,
-                          child: Center(child: Text('6')),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      fit: FlexFit.tight,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Colors.yellow,
-                              borderRadius: BorderRadius.circular(15)),
-                          child: SingleChildScrollView(
+              Expanded(
+                flex: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(15)),
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(15)),
+                            height: 200,
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Switch(
-                                    activeColor: Colors.green,
-                                    activeTrackColor: Colors.red,
-                                    value: switchOn,
-                                    onChanged: (one) {
-                                      setState(() {
-                                        switchOn = !switchOn;
-                                        print(switchOn);
-                                      });
-                                    }),
                                 TextWidget(
-                                  name: 'Azizul Hakim',
+                                  name: "Salman",
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  name: "Salman",
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  name: "Salman",
                                 ),
                               ],
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.circular(15)),
+                            height: 200,
+                            child: Center(
+                                child: TextWidget(
+                              name: "kamal",
+                            )),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(15)),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.circular(15)),
+                            // height: 80,
+                            child: Center(child: Text('6')),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.circular(15)),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Switch(
+                                      activeColor: Colors.green,
+                                      activeTrackColor: Colors.red,
+                                      value: switchOn,
+                                      onChanged: (one) {
+                                        setState(() {
+                                          switchOn = !switchOn;
+                                          print(switchOn);
+                                        });
+                                      }),
+                                  TextWidget(
+                                    name: 'Azizul Hakim',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
